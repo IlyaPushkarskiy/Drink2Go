1f0e9b1120cf121dbda7694f66c0d8d468ac82d4
IlyaPushkarskiy <121087479+IlyaPushkarskiy@users.noreply.github.com>
2023-10-17 10:12:42 +0000

    Deploying to gh-pages from @ IlyaPushkarskiy/Drink2Go@f516faf63913aaae842e1b66c2202d7ccdf5c5a3 🚀

--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-/* в этот файл добавляет скрипты*/
+const navMain=document.querySelector(".main-nav"),headerMain=document.querySelector(".main-header"),navToggle=headerMain.querySelector(".main-nav__toggle");navToggle.addEventListener("click",(()=>{navMain.classList.contains("main-nav--closed")?(navMain.classList.remove("main-nav--closed"),navMain.classList.add("main-nav--opened")):(navMain.classList.add("main-nav--closed"),navMain.classList.remove("main-nav--opened"))}));
