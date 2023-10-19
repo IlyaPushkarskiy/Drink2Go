2b89a768732cf509efc4838a56a76b9e85bc16ae
IlyaPushkarskiy <p_ilya_007@mail.ru>
2023-10-19 12:43:52 +0300

    version1 day4

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,24 @@ navToggle.addEventListener('click', () => {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+let offset = 0;
+const sliderLine = document.querySelector('.slider__wrapper');
+const nextButton = document.querySelector('.slider__next');
+const prevButton = document.querySelector('.slider__prev');
+
+nextButton.addEventListener('click', () =>{
+  offset += 320;
+  if (offset > 640) {
+    offset = 0;
+  }
+  sliderLine.style.left = `${-offset }px`;
+});
+
+prevButton.addEventListener('click', () =>{
+  offset -= 320;
+  if (offset < 0) {
+    offset = 640;
+  }
+  sliderLine.style.left = `${-offset }px`;
+});
